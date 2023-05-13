6920186762947f7ac255ed0936ebcef5a3f666de
LySeriuos <drungilas.p@gmail.com>
2023-05-13 19:48:13 +0200

    moved method to Logic class

--- QuizMaker/Logic.cs
+++ QuizMaker/Logic.cs
@@ -2,6 +2,14 @@
 {
     internal class Logic
     {
-        
+        public static UserQuestionsAndAnswers GetRandomQuestion(string path)
+        {
+            var random = new Random();
+            UserQuestionsAndAnswers questionsForUser = new UserQuestionsAndAnswers();
+            List<UserQuestionsAndAnswers> qNaList = Data.GetQnAListToXml(path);
+            int index = random.Next(qNaList.Count);
+            questionsForUser = qNaList[index];
+            return questionsForUser;
+        }
     }
 }
